3512d40dde78398e36229b4a1bcc8aa222579229
Wei Kian Chen <chen@champlain.edu>
2015-04-21 10:33:56 -0400

    updated code

--- code/final.cpp
+++ code/final.cpp
@@ -1,29 +1,17 @@
-/*
-Class: CSI-240-06
-Assignment: FINAL PROJECT
-Date Assigned: 3/17/15
-Due Date: 4/24/15 11:00AM
-
-Description:
-A class for organizing table data for a restaurant program
-
-Certification of Authenticity:
-We certify that this is entirely our own work, except where we have given fully
-documented references to the work of others. We understand the definition and
-consequences of plagiarism and acknowledge that the assessor of this assignment
-may, for the purpose of assessing this assignment:
--Reproduce this assignment and provide a copy to another member of
-academic staff; and/or
--Communicate a copy of this assignment to a plagiarism checking service
-(which may then retain a copy of this assignment on its database for
-the purpose of future plagiarism checking)
-*/
-
 #include "header.h"
+
+
+// this need to be changed;  wkc
+const int TMP_NUM = 10;
 
 int main()
 {
-    mainMenu();
-
+    Manager manager;
+    Host host[TMP_NUM];
+    Table table[TMP_NUM];
+    Kitchen kitchen[TMP_NUM];
+    
+    mainMenu(table, kitchen, manager, host);
+    
 	return 0;
 }
